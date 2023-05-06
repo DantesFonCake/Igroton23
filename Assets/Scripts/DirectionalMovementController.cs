04a4cb66b129004833be26810d1681998f767531
DantesFonCake <aripovsergey3@gmail.com>
2023-05-06 23:10:07 +0500

    less wip movement, rotation, color change

--- Assets/Scripts/DirectionalMovementController.cs
+++ Assets/Scripts/DirectionalMovementController.cs
@@ -1,13 +1,16 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class DirectionalMovementController : MonoBehaviour
 {
-    [Header("Movement Settings")] public float AccelerationFactor = 1;
-    public float Drag = 1;
+    [Header("Movement Settings")] public float accelerationFactor = 1;
+    public float drag = 1;
+    public float maxSpeed = 10;
 
     private Rigidbody2D boatBody;
     private Vector2 acceleration;
+
     public Direction LastDirection { get; private set; }
 
     private void Awake()
@@ -17,24 +20,43 @@ public class DirectionalMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        boatBody.AddForce(acceleration * AccelerationFactor, ForceMode2D.Force);
-        boatBody.drag = acceleration.SqrMagnitude() == 0 ? Drag : 0;
+        boatBody.AddForce(acceleration * accelerationFactor, ForceMode2D.Force);
+        boatBody.velocity = Vector2.ClampMagnitude(boatBody.velocity, maxSpeed);
+        boatBody.drag = acceleration.SqrMagnitude() == 0 ? drag : 0;
+        boatBody.MoveRotation(LastDirection.GetRotationAngle());
     }
 
     public void ApplyDirectionalAcceleration(float horizontalMagnitude, float verticalMagnitude)
     {
-        acceleration = new Vector2(horizontalMagnitude, verticalMagnitude);
+        var newAcceleration = new Vector2(horizontalMagnitude, verticalMagnitude);
 
-        if (horizontalMagnitude == 0 && verticalMagnitude == 0)
+        ApplyDirectionChange(horizontalMagnitude, verticalMagnitude);
+
+        acceleration = newAcceleration;
+    }
+
+    private void ApplyDirectionChange(float horizontalMagnitude, float verticalMagnitude)
+    {
+        if (horizontalMagnitude == 0 && verticalMagnitude == 0) 
             return;
+        
+        var vDirection = verticalMagnitude > 0 ? Direction.Up : Direction.Down;
+        var hDirection = horizontalMagnitude > 0 ? Direction.Right : Direction.Left;
+        var hAbsMagnitude = Mathf.Abs(horizontalMagnitude);
+        var vAbsMagnitude = Mathf.Abs(verticalMagnitude);
+        if (!Mathf.Approximately(hAbsMagnitude, vAbsMagnitude))
+        {
+            LastDirection = hAbsMagnitude > vAbsMagnitude ? hDirection : vDirection;
+            return;
+        }
 
-        if (horizontalMagnitude > 0)
-            LastDirection = Direction.Right;
-        else if (verticalMagnitude > 0)
-            LastDirection = Direction.Up;
-        else if (verticalMagnitude < 0)
-            LastDirection = Direction.Down;
-        else
-            LastDirection = Direction.Left;
+        if (Math.Sign(acceleration.x) != Math.Sign(horizontalMagnitude))
+        {
+            LastDirection = hDirection;
+            return;
+        }
+
+        if (Math.Sign(acceleration.y) != Math.Sign(verticalMagnitude))
+            LastDirection = vDirection;
     }
 }
