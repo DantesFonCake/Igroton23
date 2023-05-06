801564198ee3e12687bbe362adc2ac9a86d3381c
DantesFonCake <aripovsergey3@gmail.com>
2023-05-07 01:07:49 +0500

    better player tracking

--- Assets/Scripts/PlayerTracker.cs
+++ Assets/Scripts/PlayerTracker.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTracker : MonoBehaviour
 {
     [Header("Tracker Settings")]
     public Camera followingCamera;
+    public float followSpeed = 2;
+    public float maxFollowLag = 10;
 
     private void Update()
     {
         var selfPosition = transform.position;
         var cameraTransform = followingCamera.transform;
-        var newCameraPosition = new Vector3(selfPosition.x, selfPosition.y, cameraTransform.position.z);
+        var cameraPosition = cameraTransform.position;
+        var requiredCameraPosition = new Vector3(selfPosition.x, selfPosition.y, cameraPosition.z);
 
-        cameraTransform.position = newCameraPosition;
+        var reverseDirection = cameraPosition - requiredCameraPosition;
+
+        var sqrDistanceToLagCircle = reverseDirection.sqrMagnitude - maxFollowLag * maxFollowLag;
+        if (sqrDistanceToLagCircle > 0)
+        {
+            cameraTransform.position = cameraPosition + Vector3.ClampMagnitude(reverseDirection, maxFollowLag) - reverseDirection;
+            return;
+        }
+
+        cameraTransform.position = Vector3.MoveTowards(cameraPosition, requiredCameraPosition, followSpeed * Time.deltaTime);
     }
 }
